0f0206e6696c6e10bd0d134c4d0c6acc276e60c2
Ryanjlowe <ryanjlowe@Ryans-MacBook-Pro.local>
2013-08-13 08:05:25 -0400

    Update Objec Samples for Organization

--- objsamples/sample-organization.php
+++ objsamples/sample-organization.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('America/New_York');
 require('../ET_Client.php');
 try {	
 	$params = array();		
@@ -9,13 +9,14 @@ try {
 	print "Retrieve All Organizations with GetMoreREsults \n";
 	$getOrganization = new ET_Organization();
 	$getOrganization->authStub = $myclient;
-	$getOrganization->props = array("ID", "Client.ID", "Name", "AccountType", "AccountUsers", "Address", "BrandID", "BusinessName", "Children", "City", "Country", "DeletedDate", "EditionID", "Email", "Fax", "FromName", "InheritAddress", "IsActive", "IsTestAccount", "IsTrialAccount", "LanguageLocale", "Locale", "OrgID", "ParentAccount.ID", "ParentID", "ParentName", "Phone", "PrivateLabelID", "PrivateLabels", "Roles", "State", "TimeZone", "Zip", "CreatedDate", "ModifiedDate", "Client.ModifiedBy", "ObjectID", "CustomerKey", "Client.EnterpriseID", "Client.CreatedBy");
+	$getOrganization->props = array("ID", "Name", "AccountType", "Address", "BrandID", "BusinessName", "City", "Country", "DeletedDate", "EditionID", "Email", "Fax", "FromName", "InheritAddress", "IsActive", "IsTestAccount", "IsTrialAccount", "ParentAccount.ID", "ParentID", "ParentName", "Phone", "PrivateLabelID", "Roles", "State", "Zip", "CreatedDate", "ModifiedDate", "CustomerKey", "Client.EnterpriseID");
 	$getResponse = $getOrganization->get();
 	print_r('Get Status: '.($getResponse->status ? 'true' : 'false')."\n");
 	print 'Code: '.$getResponse->code."\n";
 	print 'Message: '.$getResponse->message."\n";
 	print_r('More Results: '.($getResponse->moreResults ? 'true' : 'false')."\n");
 	print 'Results Length: '. count($getResponse->results)."\n";
+	print 'Results: '.print_r($getResponse->results)."\n";
 	print "\n---------------\n";
 	
 	while ($getResponse->moreResults) {
@@ -30,6 +31,7 @@ try {
 	}
 
 	
+	$CustomerKeyofExistingOrganization = '1CC25B77-8BF9-4BF9-A718-168928AB0607';
 	$CustomerKeyOfTestOrganization = "TestOrganizationCustomerKey";
 	$NameOfTestOrganization = "TestOrganizationName";
 
@@ -39,7 +41,7 @@ try {
 	$getOrganization = new ET_Organization();
 	$getOrganization->authStub = $myclient;
 	$getOrganization->props = array("ID", "Name");
-	$getOrganization->filter = array('Property' => 'CustomerKey','SimpleOperator' => 'equals','Value' => $CustomerKeyOfTestOrganization);
+	$getOrganization->filter = array('Property' => 'CustomerKey','SimpleOperator' => 'equals','Value' => $CustomerKeyofExistingOrganization);
 	$getResponse = $getOrganization->get();
 	print_r('Get Status: '.($getResponse->status ? 'true' : 'false')."\n");
 	print 'Code: '.$getResponse->code."\n";
@@ -58,7 +60,7 @@ try {
 	print "Create Organization \n";
 	$postOrganization = new ET_Organization();
 	$postOrganization->authStub = $myclient;
-	$postOrganization->props = array("CustomerKey" =>  $CustomerKeyOfTestOrganization, "Name" => $NameOfTestOrganization, "Email" => "test@organization.com", "FromName" => "AGENCY CLIENT", "Business Name" => "Test Organization", "Address" => "123 ABC Street", "City" => "Indianapolis", "State" => "IN", "Zip" => "46202", "IsTestAccount" => true, "EditionID" => 3, "IsActive" => true);
+	$postOrganization->props = array("CustomerKey" =>  $CustomerKeyOfTestOrganization, "Name" => $NameOfTestOrganization, "AccountType" => "PRO_CONNECT", "Email" => "test@organization.com", "FromName" => "AGENCY CLIENT", "Business Name" => "Test Organization", "Address" => "123 ABC Street", "City" => "Indianapolis", "State" => "IN", "Zip" => "46202", "IsTestAccount" => true, "EditionID" => 3, "IsActive" => true);
 	$postResult = $postOrganization->post();
 	print_r('Post Status: '.($postResult->status ? 'true' : 'false')."\n");
 	print 'Code: '.$postResult->code."\n";
@@ -88,12 +90,11 @@ try {
 	print_r('Test Organization ID: '.$TestOrganizationID.', Name: '.$TestOrganizationName."\n");
 	
 	
-	
 	// Update Organization
 	print "Update Organization \n";
 	$patchOrganization = new ET_Organization();
 	$patchOrganization->authStub = $myclient;
-	$patchOrganization->props = array("CustomerKey" => $CustomerKeyOfTestOrganization, "Name" => "New TestOrganizationName");
+	$patchOrganization->props = array("CustomerKey" =>  $CustomerKeyOfTestOrganization, "Name" => "New TestOrganizationName", "AccountType" => "PRO_CONNECT", "Email" => "test@organization.com", "FromName" => "AGENCY CLIENT", "Business Name" => "Test Organization", "Address" => "123 ABC Street", "City" => "Indianapolis", "State" => "IN", "Zip" => "46202", "IsTestAccount" => true, "EditionID" => 3, "IsActive" => true);
 	$patchResult = $patchOrganization->patch();
 	print_r('Patch Status: '.($patchResult->status ? 'true' : 'false')."\n");
 	print 'Code: '.$patchResult->code."\n";
@@ -102,7 +103,7 @@ try {
 	print 'Results: '."\n";
 	print_r($patchResult->results);
 	print "\n---------------\n";
-	
+
 	
 	
 	// Retrieve updated Organization
@@ -110,7 +111,7 @@ try {
 	$getOrganization = new ET_Organization();
 	$getOrganization->authStub = $myclient;
 	$getOrganization->props = array("ID", "Name");
-	$getOrganization->filter = array('Property' => 'CustomerKey','SimpleOperator' => 'equals','Value' => $CustomerKeyOfTestOrganization);
+	//$getOrganization->filter = array('Property' => 'CustomerKey','SimpleOperator' => 'equals','Value' => $CustomerKeyOfTestOrganization);
 	$getResponse = $getOrganization->get();
 	print_r('Get Status: '.($getResponse->status ? 'true' : 'false')."\n");
 	print 'Code: '.$getResponse->code."\n";
@@ -129,7 +130,7 @@ try {
 	print "Delete Organization \n";
 	$deleteOrganization = new ET_Organization();
 	$deleteOrganization->authStub = $myclient;
-	$deleteOrganization->props = array("CustomerKey" => $CustomerKeyOfTestOrganization);
+	$deleteOrganization->props = array("CustomerKey" => $CustomerKeyOfTestOrganization, "AccountType" => "PRO_CONNECT");
 	$deleteResponse = $deleteOrganization->delete();
 	print_r('Delete Status: '.($deleteResponse->status ? 'true' : 'false')."\n");
 	print 'Code: '.$deleteResponse->code."\n";
